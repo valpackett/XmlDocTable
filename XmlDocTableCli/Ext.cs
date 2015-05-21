7171e6a42b1805def3bb40a8ca7247b09991a62f
Greg V <greg@unrelenting.technology>
2015-05-22 02:27:39 +0300

    strip xml

--- XmlDocTableCli/Ext.cs
+++ XmlDocTableCli/Ext.cs
@@ -1,4 +1,5 @@
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -8,11 +9,13 @@ namespace XmlDocTableCli
     {
         public static string CommentField(this SyntaxNode doc, string field)
         {
-            return string.Join("",
+            var s = string.Join("",
                 doc.DescendantNodes()
                     .OfType<XmlElementSyntax>()
                     .Where(x => x.StartTag.Name.LocalName.ValueText.Contains(field))
-                    .Select(x => x.Content.ToString())).Replace("///", "").Trim();
+                    .Select(x => x.Content.ToString()));
+            s = Regex.Replace(s, @"<[^>]+>", "").Replace("///", "").Trim();
+            return s;
         }
     }
 }
